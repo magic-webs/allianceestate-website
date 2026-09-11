66b493672b0f67d5c1dcbb15b975c7a918e329e7
Jatin Jain <promptxjatin@gmail.com>
2026-09-11 17:05:22 +0530

    feat: add Hello World header to main landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* Start your page here */}
+      <h1>Hello World</h1>
     </div>
   );
 }
